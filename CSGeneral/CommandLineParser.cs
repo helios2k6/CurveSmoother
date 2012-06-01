e24629aec842ad39d2fe8826bb9fbed30e8193bc
helios2k6 <helios2k6@gmail.com>
2012-06-01 10:05:11 -0400

    added commandline parser

---
+++ CSGeneral/CommandLineParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSGeneral
+{
+	public class CommandLineParser
+	{
+		private readonly IDictionary<string, string> _parsingDictionary = new Dictionary<string, string>();
+		private readonly IDictionary<string, string> _invalidOptions = new Dictionary<string, string>();
+
+		public CommandLineParser(IList<string> commandArgs, IList<string> commandLineOptions, IList<string> deliminators = null)
+		{
+			foreach (var s in commandArgs)
+			{
+				if (commandLineOptions.Contains(s))
+				{
+
+				}
+			}
+		}
+
+		public string this[string param]
+		{
+			get
+			{
+				string result;
+
+				return _parsingDictionary.TryGetValue(param, out result) ? result : string.Empty;
+			}
+		}
+	}
+}
