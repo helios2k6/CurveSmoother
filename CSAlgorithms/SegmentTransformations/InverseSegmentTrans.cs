2a46d0d56279a1f2e48c0de006eb38b3d090e6cf
helios2k6 <helios2k6@gmail.com>
2012-06-12 17:50:58 -0400

    added inverse interp

---
+++ CSAlgorithms/SegmentTransformations/InverseSegmentTrans.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSModel.Interfaces;
+using CSModel.Model;
+
+namespace CSAlgorithms.SegmentTransformations
+{
+	public class InverseSegmentTransformation : ICurveSegmentTransformation
+	{
+		private Func<IEnumerable<CurveSegment>, CurveSegment> _evaluationPredicate;
+
+		public InverseSegmentTransformation(Func<IEnumerable<CurveSegment>, CurveSegment> evaluationPredicate)
+		{
+			_evaluationPredicate = evaluationPredicate;
+		}
+
+		public CurveSegment Transform(CurveSegment curveSegment)
+		{
+			var dataPointsAsList = curveSegment.DataPoints.ToList();
+
+			var numDataPoints = dataPointsAsList.Count;
+			if (numDataPoints < 3) throw new ArgumentException("Cannot have less than 3 data points");
+
+			var firstDataPoint = curveSegment.DataPoints.First();
+			var lastDataPoint = curveSegment.DataPoints.Last();
+
+			dataPointsAsList.RemoveAt(numDataPoints - 1);
+			dataPointsAsList.RemoveAt(0);
+
+			var allDataPoints = from n in dataPointsAsList.AsParallel()
+								select SolveInverseCurve(new List<DataPoint> { firstDataPoint, n, lastDataPoint });
+			return _evaluationPredicate(allDataPoints);
+		}
+
+		private CurveSegment SolveInverseCurve(IList<DataPoint> dataPoints)
+		{
+
+		}
+	}
+}
